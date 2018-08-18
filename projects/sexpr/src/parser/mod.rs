ade3129a6d6ff8956ca26d00e2f5b8a54e17bf4e
Aster <aster@vers.site>
2018-08-18 09:46:07 +0000

    🐣 Parsing the list

--- projects/sexpr/src/parser/mod.rs
+++ projects/sexpr/src/parser/mod.rs
@@ -3,22 +3,35 @@ use std::str::FromStr;
 use pex::{ParseResult, ParseState};
 use pex::helpers::decimal_string;
 
-pub struct SExpr {}
+#[derive(Clone, Debug)]
+pub enum SExpr {
+    Number(SNumber),
+    List(Box<SList>),
+    Symbol(String),
+    String(String),
+}
 
 #[derive(Clone, Debug)]
-pub struct Number {
+pub struct SList {
+    value: Vec<SExpr>,
+    range: Range<usize>,
+}
+
+#[derive(Clone, Debug)]
+pub struct SNumber {
     value: f64,
     range: Range<usize>,
 }
 
-
 impl SExpr {
-    pub fn parse(&self, state: ParseState) -> ParseResult<()> {}
+    pub fn parse(&self, state: ParseState) -> ParseResult<()> {
+        todo!()
+    }
 }
 
 
-impl Number {
-    pub fn parse(&self, input: ParseState) -> ParseResult<Self> {
+impl SNumber {
+    pub fn parse(input: ParseState) -> ParseResult<Self> {
         let (state, s) = decimal_string(input)?;
         let value = f64::from_str(s)?;
         let range = state.away_from(input);
